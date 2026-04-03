46b344b7340f97c17cd9ed787116e3b2f819f74d
Omar Enrique <omar.e.geek@gmail.com>
2026-04-03 03:22:53 -0400

    First Commit

---
+++ src/app/pages/contact-page/contact-page.ts
@@ -0,0 +1,30 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'contact-page',
+  imports: [],
+  templateUrl: './contact-page.html',
+})
+export default class ContactPage implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('Contact Page');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este es mi Contact Page',
+    });
+
+    this.meta.updateTag({
+      name: 'og:title',
+      content: 'About Page',
+    });
+
+    this.meta.updateTag({
+      name: 'keywords',
+      content: 'Hola,Mundo,Omar,Enrique,Curso,Angular,PRO',
+    });
+  }
+}
---
+++ src/app/pages/pricing-page/pricing-page.ts
@@ -0,0 +1,30 @@
+import { isPlatformServer } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'pricing-page',
+  imports: [],
+  templateUrl: './pricing-page.html',
+})
+export default class PricingPage implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+  // private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    // if (!isPlatformServer(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este es mi Pricing Page',
+    });
+
+    this.meta.updateTag({
+      name: 'og:title',
+      content: 'About Page',
+    });
+  }
+}
