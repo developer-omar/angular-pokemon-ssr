01a00f44d0526580453e0f895ae99ccf34a710cd
Omar Enrique <omar.e.geek@gmail.com>
2026-04-15 01:00:15 -0400

    fixing some routes for netlify

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,26 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+const POKEMON_LIMIT = 151;
+
+async function fetchPokemonNames(limit: number): Promise<string[]> {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}`,
+  );
+  const data = await response.json();
+  return data.results.map((pokemon: { name: string }) => pokemon.name);
+}
+
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const names = await fetchPokemonNames(POKEMON_LIMIT);
+      return names.map((name) => ({ id: name }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
